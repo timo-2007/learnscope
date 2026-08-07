dbcdbbc51cd66fa253ff1967337e15e011da5117
timo-2007 <timo.vollhardt@gmail.com>
2026-08-07 21:53:31 +0200

    Add admin logout button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,15 +2,24 @@ import { Component, inject } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { AuthService } from '../core/auth.service';
 import { RouterLink } from '@angular/router';
-import { IonButtons, IonIcon } from '@ionic/angular/standalone';
+import { IonButtons, IonIcon, IonButton } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { lockClosedOutline, logOutOutline } from 'ionicons/icons'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonIcon, RouterLink],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, RouterLink],
 })
 export class HomePage {
-  auth = inject(AuthService)
-  constructor() {}
+  auth = inject(AuthService);
+
+  constructor() {
+    addIcons({ lockClosedOutline, logOutOutline });
+  }
+
+  logout() {
+    this.auth.logout();
+  }
 }
