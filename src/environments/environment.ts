7045fcd1a0d9fa91a6cbb541e3936ab47dd470c3
timo-2007 <timo.vollhardt@gmail.com>
2026-08-07 20:52:07 +0200

    Add Supabase config to environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabaseUrl: 'https://cidrhbxvwlugecjljpuy.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpZHJoYnh2d2x1Z2VjamxqcHV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMTc2NjMsImV4cCI6MjEwMTY5MzY2M30.ZMuK2v-c5s0VX-NoMekyLOUyV0e9Gfucq53f8YmWZco',
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
