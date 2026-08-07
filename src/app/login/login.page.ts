d0c7e82773e416aabc5d7a01d16a6cc64fc97d5d
timo-2007 <timo.vollhardt@gmail.com>
2026-08-07 20:52:33 +0200

    Add login page

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,31 @@
+import { Component, inject, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { 
+  IonCardHeader, IonItem, IonLabel, IonInput,
+  IonButton, IonContent, IonHeader, IonTitle, IonToolbar,
+} from '@ionic/angular/standalone';
+import { AuthService } from '../core/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  imports: [IonItem, IonLabel, IonInput, IonContent, IonHeader, IonTitle, IonToolbar, IonButton, FormsModule],
+})
+export class LoginPage {
+  private auth = inject(AuthService);
+  private router = inject(Router);
+
+  email = '';
+  password = '';
+  error = signal('');
+
+  async login() {
+    const { error } = await this.auth.login(this.email, this.password);
+    if (error) {
+      this.error.set(error.message);
+      return;
+    }
+    this.router.navigateByUrl('/admin');
+  }
+}
