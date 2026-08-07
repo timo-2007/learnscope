1ccbda719b4d37e2c9f685d5874371b46ab3bfa4
timo-2007 <timo.vollhardt@gmail.com>
2026-08-07 23:10:23 +0200

    Add side menu shell to app root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import {
+  IonApp, IonRouterOutlet, IonSplitPane, IonMenu, IonMenuToggle,
+  IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonIcon, IonLabel,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { homeOutline, libraryOutline, shieldCheckmarkOutline, lockClosedOutline, logOutOutline } from 'ionicons/icons';
+import { AuthService } from './core/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [
+    RouterLink, RouterLinkActive,
+    IonApp, IonRouterOutlet, IonSplitPane, IonMenu, IonMenuToggle,
+    IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonIcon, IonLabel,
+  ],
 })
 export class AppComponent {
-  constructor() {}
+  auth = inject(AuthService);
+
+  constructor() {
+    addIcons({ homeOutline, libraryOutline, shieldCheckmarkOutline, lockClosedOutline, logOutOutline });
+  }
+
+  logout() {
+    this.auth.logout();
+  }
 }
