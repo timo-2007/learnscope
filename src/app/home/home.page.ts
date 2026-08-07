7c3c3b0d0a664a4f354751f69d73902bad287a6f
timo-2007 <timo.vollhardt@gmail.com>
2026-08-07 22:06:27 +0200

    Moved Logout and login button to general top-bar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,25 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { AuthService } from '../core/auth.service';
-import { RouterLink } from '@angular/router';
-import { IonButtons, IonIcon, IonButton } from '@ionic/angular/standalone';
-import { addIcons } from 'ionicons';
-import { lockClosedOutline, logOutOutline } from 'ionicons/icons'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, RouterLink],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
-export class HomePage {
-  auth = inject(AuthService);
-
-  constructor() {
-    addIcons({ lockClosedOutline, logOutOutline });
-  }
-
-  logout() {
-    this.auth.logout();
-  }
-}
+export class HomePage {}
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,23 +1,30 @@
 import { Component, inject } from '@angular/core';
-import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import {
+  IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel,
+  IonHeader, IonToolbar, IonTitle, IonButtons, IonButton,
+} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { homeOutline, libraryOutline, lockClosedOutline, shieldCheckmarkOutline } from 'ionicons/icons';
+import { homeOutline, libraryOutline, lockClosedOutline, logOutOutline, shieldCheckmarkOutline } from 'ionicons/icons';
 import { AuthService } from '../core/auth.service';
-
-addIcons({ homeOutline, libraryOutline });
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+  imports: [
+    RouterLink,
+    IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel,
+    IonHeader, IonToolbar, IonTitle, IonButtons, IonButton,
+  ],
 })
 export class TabsPage {
   auth = inject(AuthService);
 
   constructor() {
-    addIcons({ homeOutline });
-    addIcons({ libraryOutline });
-    addIcons({ shieldCheckmarkOutline });
-    addIcons({ lockClosedOutline })
+    addIcons({ homeOutline, libraryOutline, shieldCheckmarkOutline, lockClosedOutline, logOutOutline });
+  }
+
+  logout() {
+    this.auth.logout();
   }
 }
